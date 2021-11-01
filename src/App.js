3f2b25b41cebbaf3b2e907abe43f8e97dd095343
Platonishe <kagubnotalp@gmail.com>
2021-11-01 13:22:31 +0300

    разнес компоненты в разные директории для удобства скрипта, наладил css

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
       <div className='app-wrapper'>
-        <header className='header'>
-            <img src='https://e7.pngegg.com/pngimages/547/294/png-clipart-swastika-croix-gammee-nazie-nazism-nazi-germany-operation-paperclip-others-angle-text.png' />
-        </header>
-          <nav className='nav'>
-              <div>
-                 <a>Profile</a>
-              </div>
-              <div>
-                  <a>Messages</a>
-              </div>
-              <div>
-                  <a>News</a>
-              </div>
-              <div>
-                  <a>Music</a>
-              </div>
-              <div>
-                  <a>Settings</a>
-              </div>
-          </nav>
-          <div className='content'>
-              <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYsVdzfNcTFfiyfPbK_s_TkxjI1xg-ysYFw&usqp=CAU' />
-          <div>
-              ava + desc
-          </div>
-          <div>
-              My posts
-              <div>
-                  new post
-              </div>
-              <div>
-                  <div>
-                      post 1
-                  </div>
-                   <div>
-                      post 2
-                  </div>
-             </div>
-          </div>
-          </div>
+          <Header />
+          <Navbar />
+          <Profile />
       </div>);
 }
 
