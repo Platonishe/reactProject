6c872ad56373303ab8158eaabd69629509b91509
Platonishe <kagubnotalp@gmail.com>
2021-11-04 14:01:31 +0300

    Сделал dialogs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
-/*let names = {
-    'nav': 'Navbar_nav__3VrcO',
-    'item': 'Navbar_item__Bo2WM'
-}*/
-let c1 = "item";
-let c2 = "active";
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${c2}`;
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
         </div>
         <div className={s.item}>
             <a>News</a>
